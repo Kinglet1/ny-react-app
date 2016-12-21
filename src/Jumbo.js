9815433e20bbfeed34bcc4b1ec67477542771dc2
Stanislav Vetlovskiy <mrerliz@gmail.com>
2016-12-21 16:18:42 +0300

    Refactor

--- src/Jumbo.js
+++ src/Jumbo.js
@@ -2,21 +2,16 @@ import React, { Component } from 'react';
 import './Jumbo.css';
 
 class Jumbo extends Component {
-	constructor(props) {
-		super(props);
-		//this.state = { greeting: 'Hello' };
-	}
-
 	render() {
 		return (
 			<div className="jumbo">
 				<div className="jumbo__author">
-					<img src={ this.props.post.avatar } alt="" className="jumbo__author-photo"/>
-					<div className="jumbo__author-name">{ this.props.post.name }</div>
+					<img src={ this.props.post.userpic } alt="" className="jumbo__author-photo"/>
+					<div className="jumbo__author-name">{ this.props.post.username }</div>
 				</div>
 
 				<div className="jumbo__post">
-					<img src={ this.props.post.post } alt="" width='400' height='400'/>
+					<img src={ this.props.post.image } alt="" width='400' height='400'/>
 				</div>
 			</div>
 		);
--- src/JumboList.js
+++ src/JumboList.js
@@ -1,100 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import $ from 'jquery';
 import './JumboList.css';
 import Jumbo from './Jumbo';
 
-import createMockjax from 'jquery-mockjax';
-const mockjax = createMockjax($, window);
-
-var testPosts = [{
-	avatar: 'https://pp.vk.me/c638222/v638222121/fd35/jtOfNd2OUjE.jpg',
-	name: 'Jack Sparrow',
-	post: 'https://pp.vk.me/c635102/v635102436/16c7f/Knut8RJZFaE.jpg'
-}, {
-	avatar: 'https://pp.vk.me/c638222/v638222121/fcec/k2VyfY91UyY.jpg',
-	name: 'Red Riding Hood',
-	post: 'https://pp.vk.me/c635102/v635102436/16c83/Icj6UJyO6gc.jpg'
-}, {
-	avatar: 'https://pp.vk.me/c638222/v638222121/fcfc/LozqScJwPpo.jpg',
-	name: 'Bembi',
-	post: 'https://pp.vk.me/c635102/v635102436/16c97/iBDIpxWK_6A.jpg'
-}, {
-	avatar: 'https://pp.vk.me/c638222/v638222121/fd25/UgP5mVeOB0Y.jpg',
-	name: 'Piggy',
-	post: 'https://pp.vk.me/c635102/v635102436/16ca0/QdGLXKhhwAQ.jpg'
-}];
-
-function getRandomInt(min, max)
-{
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getData() {
-	var posts = [];
-	for (var i=0; i<3; i++) {
-		posts.push(testPosts[getRandomInt(0,3)]);
-	}
-	return posts;
-}
-
-mockjax({
-	url: "/restful/fortune",
-	// responseText: {
-	// 	posts: getData()
-	// },
-	response: function() {
-		this.responseText = {
-			posts: getData()
-		};
-	}
-});
-/**/
-
 class JumboList extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { posts: [{
-			avatar: 'https://pp.vk.me/c638222/v638222121/fd35/jtOfNd2OUjE.jpg',
-			name: 'Jack Sparrow',
-			post: 'https://pp.vk.me/c635102/v635102436/16c7f/Knut8RJZFaE.jpg'
-		}, {
-			avatar: 'https://pp.vk.me/c638222/v638222121/fcec/k2VyfY91UyY.jpg',
-			name: 'Red Riding Hood',
-			post: 'https://pp.vk.me/c635102/v635102436/16c83/Icj6UJyO6gc.jpg'
-		}, {
-			avatar: 'https://pp.vk.me/c638222/v638222121/fcfc/LozqScJwPpo.jpg',
-			name: 'Bembi',
-			post: 'https://pp.vk.me/c635102/v635102436/16c97/iBDIpxWK_6A.jpg'
-		}] };
+  constructor(props) {
+    super(props);
+    this.state = {posts: []};
+    this.updateData();
+    this.onNextButtonClick = this.onNextButtonClick.bind(this);
+  }
 
-		this.onNextButtonClick = this.onNextButtonClick.bind(this);
-	}
+  updateData() {
+    let self = this;
+    $.getJSON("api.json", (response, err) =>{
+      if (err) {
+        console.log(err);
+      }
+      console.log(response);
+      self.setState({posts: response.posts});
+    });
+  }
 
-	renderJumboList(posts) {
-		return posts.map(post => (
-			< Jumbo post={post} />
-		));
-	}
+  onNextButtonClick() {
+    this.updateData()
+  }
 
-	onNextButtonClick() {
-		var self = this;
-
-		$.getJSON("/restful/fortune", function(response) {
-			console.log(response.posts);
-			self.setState({ posts: response.posts });
-		});
-	}
-
-	render() {
-		return (
-			<div className="jumbo-list">
-				<button className="jumbo-list__button" onClick={ this.onNextButtonClick }>Next</button>
-				<div className="jumbo-list__list">
-					{ this.renderJumboList(this.state.posts) }
-				</div>
-			</div>
-		)
-	}
+  render() {
+    const list = this.state.posts.map((item) => {
+      console.log(item);
+      return < Jumbo key={item.postid} post={item}/>
+    });
+    return (
+      <div className="jumbo-list">
+        <button className="jumbo-list__button" onClick={ this.onNextButtonClick }>Next</button>
+        <div className="jumbo-list__list">
+          {list}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default JumboList;
