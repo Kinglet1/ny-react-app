5311b0c31cc189684d24cd7cfbbe66eb3d10fec5
Liudmila Mzhachikh <l.mzhachikh@corp.mail.ru>
2016-12-22 19:09:34 +0300

    Add condition for empty avatar

--- src/Jumbo.js
+++ src/Jumbo.js
@@ -3,16 +3,25 @@ import './Jumbo.css';
 
 class Jumbo extends Component {
   render() {
+	var author;
+
+  	if (!this.props.post.userpic || !this.props.post.username) {
+	    author = <div className="jumbo__author"></div>
+    }
+    else {
+	    author = <div className="jumbo__author">
+		    <img src={ this.props.post.userpic } alt="" className="jumbo__author-photo"/>
+		    <div className="jumbo__author-name">{ this.props.post.username }</div>
+	    </div>
+    }
+
     return (
       <div className="jumbo">
         <div className="jumbo__post">
           <img className="jumbo__post-img" src={ this.props.post.image } alt=""/>
         </div>
 
-	      <div className="jumbo__author">
-		      <img src={ this.props.post.userpic } alt="" className="jumbo__author-photo"/>
-		      <div className="jumbo__author-name">{ this.props.post.username }</div>
-	      </div>
+	    { author }
       </div>
     );
   }
