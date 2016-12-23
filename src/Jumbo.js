78cc0e5c11f6f2e9052a60a58067f38ee47df58b
Liudmila Mzhachikh <l.mzhachikh@corp.mail.ru>
2016-12-23 12:53:30 +0300

    Fix text, add animation

--- src/Jumbo.js
+++ src/Jumbo.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import './Jumbo.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 class Jumbo extends Component {
   render() {
+  	var text = this.props.post.text || '';
+	if (text.length > 200) {
+		text = text.substring(0, 200) + '...';
+	}
+
 	var author;
 
   	if (!this.props.post.userpic || !this.props.post.username) {
 	    author = <div className="jumbo__author">
 		    <div className="jumbo__post-text">
-			    { this.props.post.text.substring(0, 250) + '...' }
+			    { text }
 		    </div>
 	    </div>
     }
@@ -18,21 +24,30 @@ class Jumbo extends Component {
 		    <div className="jumbo__author-text">
 			    <div className="jumbo__author-name">{ this.props.post.username } </div>
 			    <div className="jumbo__post-text">
-				    { this.props.post.text.substring(0, 250) + '...' }
+				    { text }
 			    </div>
 		    </div>
 	    </div>
     }
 
     return (
-      <div className="jumbo">
+	    <ReactCSSTransitionGroup
+		    transitionName="example"
+		    transitionLeave={false}
+		    transitionLeaveTimeout={1500}
+		    transitionEnter={true}
+		    transitionEnterTimeout={1500}>
 
-        <div className="jumbo__post">
-          <img className="jumbo__post-img" src={ this.props.post.image } alt=""/>
-        </div>
+		      <div className="jumbo">
 
-	    { author }
-      </div>
+		        <div className="jumbo__post">
+		          <img className="jumbo__post-img" src={ this.props.post.image } alt=""/>
+		        </div>
+
+			    { author }
+		      </div>
+
+		    </ReactCSSTransitionGroup>
     );
   }
 }
