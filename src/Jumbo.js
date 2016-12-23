73af58ab1a1b0c6a2f21fb99a499cf2f9a04670a
Liudmila Mzhachikh <l.mzhachikh@corp.mail.ru>
2016-12-23 12:17:59 +0300

    Add post text

--- src/Jumbo.js
+++ src/Jumbo.js
@@ -6,17 +6,27 @@ class Jumbo extends Component {
 	var author;
 
   	if (!this.props.post.userpic || !this.props.post.username) {
-	    author = <div className="jumbo__author"></div>
+	    author = <div className="jumbo__author">
+		    <div className="jumbo__post-text">
+			    { this.props.post.text.substring(0, 250) + '...' }
+		    </div>
+	    </div>
     }
     else {
 	    author = <div className="jumbo__author">
 		    <img src={ this.props.post.userpic } alt="" className="jumbo__author-photo"/>
-		    <div className="jumbo__author-name">{ this.props.post.username }</div>
+		    <div className="jumbo__author-text">
+			    <div className="jumbo__author-name">{ this.props.post.username } </div>
+			    <div className="jumbo__post-text">
+				    { this.props.post.text.substring(0, 250) + '...' }
+			    </div>
+		    </div>
 	    </div>
     }
 
     return (
       <div className="jumbo">
+
         <div className="jumbo__post">
           <img className="jumbo__post-img" src={ this.props.post.image } alt=""/>
         </div>
